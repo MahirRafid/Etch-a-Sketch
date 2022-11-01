49f95964ce0b8c4a860caccdc9aee320985e1f27
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-11-01 22:34:20 +0600

    Added eraser and clear button functions

--- script.js
+++ script.js
@@ -6,14 +6,42 @@ slider.oninput = function () {
   setupGrid(sliderValue);
   console.log(sliderValue);
 };
-// console.log(sliderValue);
+
+// Initial Grid Setup //
+
+let x = 10;
+setupGrid(x);
+
+//Grid setup using the slider//
 
 function setupGrid(x) {
+  clearFunc(x);
   for (let i = 0; i < x; i++) {
     let main = document.createElement("div");
     main.classList.add("main");
     for (let j = 0; j < x; j++) {
       let box = document.createElement("div");
+      box.classList.add("boxStyle");
+      box.classList.add("box");
+      box.style.width = `${540 / x}px`;
+      box.style.height = `${540 / x}px`;
+      box.addEventListener("mouseover", (e) => box.classList.add("colorBoxBW"));
+      main.append(box);
+    }
+    let square = document.querySelector(".square");
+    square.append(main);
+  }
+}
+
+//Clear grid//
+
+function setupGridAfterClear(x) {
+  for (let i = 0; i < x; i++) {
+    let main = document.createElement("div");
+    main.classList.add("main");
+    for (let j = 0; j < x; j++) {
+      let box = document.createElement("div");
+      box.classList.add("boxStyle");
       box.classList.add("box");
       box.style.width = `${540 / x}px`;
       box.style.height = `${540 / x}px`;
@@ -25,20 +53,34 @@ function setupGrid(x) {
   }
 }
 
-// Initial Grid Setup //
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearBtnFunc);
 
-let x = 20;
-for (let i = 0; i < x; i++) {
-  let main = document.createElement("div");
-  main.classList.add("main");
-  for (let j = 0; j < x; j++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    box.style.width = `${540 / x}px`;
-    box.style.height = `${540 / x}px`;
-    box.addEventListener("mouseover", (e) => box.classList.add("colorBox"));
-    main.append(box);
-  }
-  let square = document.querySelector(".square");
-  square.append(main);
+function clearFunc(x) {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.style.width = "0px";
+    box.style.height = "0px";
+  });
+  // setupGridAfterClear(x);
 }
+
+function clearBtnFunc() {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.style.backgroundColor = "rgb(248, 247, 247)";
+  });
+}
+
+// Eraser function //
+
+let eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", (e) => {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener(
+      "click",
+      (e) => (box.style.backgroundColor = "rgb(248, 247, 247)")
+    );
+  });
+});

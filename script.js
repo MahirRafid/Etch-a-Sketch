4d8edec3a13726bce42b9669abb9a8af93be2ea5
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-11-01 09:03:34 +0600

    Added frontend designs for the website

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ for (let i = 0; i < x; i++) {
   for (let j = 0; j < x; j++) {
     let box = document.createElement("div");
     box.classList.add("box");
-    box.style.width = `${380 / x}px`;
-    box.style.height = `${380 / x}px`;
+    box.style.width = `${540 / x}px`;
+    box.style.height = `${540 / x}px`;
     box.addEventListener("mouseover", (e) => box.classList.add("colorBox"));
     main.append(box);
   }
